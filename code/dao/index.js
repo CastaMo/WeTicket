ea1f78b6da31717c0de5ae5c8dbaf10dc9eb2f44
zero-mo <502442134@qq.com>
2016-07-12 14:18:31 +0800

    add dao

---
+++ code/dao/index.js
@@ -0,0 +1,11 @@
+/**
+ * @author: CastMo
+ * @last-edit-date: 2016-07-12
+ */
+
+exports.Cinema     	= require('./cinema');
+exports.Movie  		= require('./movie');
+exports.Order     	= require('./order');
+exports.Session 	= require('./session');
+exports.Ticket  	= require('./ticket');
+exports.User    	= require('./user');
