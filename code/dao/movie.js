75fba2a7b3b416001e8efb1045e31f2c490580ad
zero-mo <502442134@qq.com>
2016-07-12 20:41:49 +0800

    modify movie

--- code/dao/movie.js
+++ code/dao/movie.js
@@ -5,12 +5,36 @@ var EventProxy 	= require("eventproxy"),
 var getMovieById = function(id, callback) {
 	Movie.findOne({_id:id}, function(err, movie) {
 		if (err) {
-			return callback(err, movie);
+			return callback(err);
 		}
 		return callback(null, movie);
 	});
 }
 
+var getAllMovies = function(callback) {
+	Movie.find(function(err, movies) {
+		if (err) {
+			return callback(err);
+		}
+		return callback(null, movies);
+	});
+}
+
+var getMoviesByType = function(type, callback) {
+	getAllMovies(function(err, movies) {
+		if (err) {
+			return callback(err);
+		}
+		var results = [];
+		for (var i = 0, len = movies.length; i < len; i++) {
+			var movie = movies[i];
+			if (movie & type) {
+				results.push(movie);
+			}
+		}
+		return callback(null, results);
+	});
+}
 
 var newAndSave = function(options, callback) {
 	var movie 			= new Movie();
@@ -20,6 +44,7 @@ var newAndSave = function(options, callback) {
 	movie.score 		= options.score;
 	movie.movie_name 	= options.movie_name;
 	movie.release_time 	= options.release_time;
+	movie.duration_time = options.duration_time;
 	movie.type 			= options.type;
 
 	movie.save(callback);
@@ -29,5 +54,7 @@ var newAndSave = function(options, callback) {
 
 module.exports = {
 	getMovieById 	: 	getMovieById,
+	getAllMovies 	: 	getAllMovies,
+	getMoviesByType : 	getMoviesByType,
 	newAndSave 		: 	newAndSave
 };
